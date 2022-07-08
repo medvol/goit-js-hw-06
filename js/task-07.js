8f3951247d3d5789736ee9fd7adb2f4adae92028
Oleh Medvid <olmedvid@gmail.com>
2022-07-08 22:15:19 +0300

    add task 7-8

--- js/task-07.js
+++ js/task-07.js
@@ -1,13 +1,14 @@
 const inputEl = document.querySelector('#font-size-control');
 const textSize = document.querySelector('#text');
 
-textSize.style.fontSize = inputEl.value;
 
- console.log(textSize.style.fontSize)
+textSize.style.fontSize = `${inputEl.value}px`;
 
 inputEl.addEventListener('input', scaleChange);
 
 function scaleChange(event) {
-    console.log(textSize.style.fontSize)
-    textSize.style.fontSize = event.currentTarget.value;
+    
+    textSize.style.fontSize = `${event.currentTarget.value}px`;
 }
+
+
--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const form = document.querySelector('.login-form');
 
+
+
+const onSubmitForm = (event) => {
+     event.preventDefault();
+  const {elements: { email, password }} = event.currentTarget;
+
+  if (email.value === '' || password.value === '') {
+    return console.log(alert('Please fill all fields!'));
+  }
+    
+    const dataForm = {
+        email: email.value,
+        password: password.value,
+    }
+
+  console.log(dataForm);
+  event.currentTarget.reset();
+}
+
+form.addEventListener('submit', onSubmitForm);
