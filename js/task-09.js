03f86c8ce555c56da23c7fc9edae8c9a98d62fe2
Oleh Medvid <olmedvid@gmail.com>
2022-07-08 23:13:43 +0300

    add task 9

--- js/task-09.js
+++ js/task-09.js
@@ -2,21 +2,17 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-
 const changeColorBtn = document.querySelector('.change-color');
 const textColor = document.querySelector('.color');
-
-console.log(getRandomHexColor());
 
 changeColorBtn.addEventListener('click', changeColor);
 
 function changeColor(element) {
 
-  const color = getRandomHexColor()
+  const color = getRandomHexColor();
 
-  document.body.style.background = color;
+  document.body.style.background = `${color}`;
 
   textColor.textContent = color;
-
-  console.log(document.body.style.background = color)
+  
 }
